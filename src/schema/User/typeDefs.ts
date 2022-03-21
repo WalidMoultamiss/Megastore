7883affe8f381439423e171925f552deb4088664
walid moultamiss <walidmoultamis@gmail.com>
2022-03-21 14:37:47 +0100

    add User

--- src/schema/User/typeDefs.ts
+++ src/schema/User/typeDefs.ts
@@ -1,8 +1,21 @@
 import { gql } from 'apollo-server-express';
 
+
+
+
 // Construct a schema, using GraphQL schema language
 export const typeDefs = gql`
+type User {
+  id: ID!
+  name: String!
+  email: String!
+  password: String!
+  createdAt: String!
+  updatedAt: String!
+}
+
   type Query {
-    hello: String
+    users: [User!]!
+    user(id: ID!): User
   }
 `;
